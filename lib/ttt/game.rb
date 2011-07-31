04606836c491df0186316a3f6b21ab7ee3ed8be1
Josh Cheek <josh.cheek@gmail.com>
2011-07-30 21:48:40 -0500

    Create a game with a current board, game knows whose turn it is

--- lib/ttt/game.rb
+++ lib/ttt/game.rb
@@ -1,7 +1,15 @@
 module TTT
   class Game
-    def board
-      '0'*9
+    
+    DEFAULT_BOARD = '0'*9
+    attr_accessor :board
+    
+    def initialize(board=DEFAULT_BOARD)
+      self.board = board
+    end
+    
+    def turn
+      board.scan('1').size - board.scan('2').size + 1
     end
   end
 end
--- spec/ttt/game_spec.rb
+++ spec/ttt/game_spec.rb
@@ -5,5 +5,14 @@ module TTT
     it 'has no moves when created without an argument' do
       Game.new.board.should == '0'*9
     end
+    
+    context 'when it is created with an unfinished board' do
+      context ' with an equal number of 1s and 2s' do
+        let(:board) { '120000000' }
+        subject { Game.new board }
+        its(:board) { should == board }
+        its(:turn) { should == 1 }
+      end
+    end
   end
 end
